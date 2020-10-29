554b30eaaa22b27ba4893c5e379a488ab5639b93
Manjot Sidhu <manjot.techie@gmail.com>
2020-10-29 16:59:36 +0530

    SpringBoot: Initial REST Service.
    Signed-off-by: Manjot Sidhu <manjot.techie@gmail.com>

---
+++ SpringBoot/src/test/java/com/manjotsidhu/springboot/SpringBootLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manjotsidhu.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
