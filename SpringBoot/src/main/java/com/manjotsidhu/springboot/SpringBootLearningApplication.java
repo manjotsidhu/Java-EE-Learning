dfb1c7f5b16a8302de6eee4330266ec000d114a1
Manjot Sidhu <manjot.techie@gmail.com>
2020-10-30 11:48:18 +0530

    SpringBoot: Consuming REST API example.
    Signed-off-by: Manjot Sidhu <manjot.techie@gmail.com>

--- SpringBoot/src/main/java/com/manjotsidhu/springboot/SpringBootLearningApplication.java
+++ SpringBoot/src/main/java/com/manjotsidhu/springboot/SpringBootLearningApplication.java
@@ -1,13 +1,38 @@
 package com.manjotsidhu.springboot;
 
+import com.manjotsidhu.springboot.consumingrest.Quote;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
 public class SpringBootLearningApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(SpringBootLearningApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootLearningApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			// Consuming REST API Service
+			Quote quote = restTemplate.getForObject(
+					"https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+			log.info(quote.toString());
+		};
 	}
 
 }
